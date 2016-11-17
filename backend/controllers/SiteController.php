a6bd2944bae36374970cee2042c832f19ab8f736
AMWOLLO VICTOR <amwollo@yahoo.com>
2016-11-17 13:31:08 +0800

    Update Site Controller

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -60,7 +60,7 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        $this->layout = 'dashboard';
+        //$this->layout = 'dashboard';
         return $this->render('index');
     }
 
@@ -96,8 +96,7 @@ class SiteController extends Controller
     {
         Yii::$app->user->logout();
 
-
         return $this->goHome();
-         
+
     }
 }
